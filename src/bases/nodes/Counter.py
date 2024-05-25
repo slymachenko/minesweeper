6c0e94ddc4ab67ac074e230e35d3d50c61d9cf9d
reset <reset>
2024-05-25 15:28:43 +0200

    Counter and Timer separated

---
+++ src/bases/nodes/Counter.py
@@ -0,0 +1,44 @@
+# TYPES
+from src.bases.nodes.Node import Node
+from src.bases.scenes.Scene import Scene
+from src.bases.nodes.AnimatedSprite import AnimatedSprite
+
+class Counter(Node):
+    def __init__(
+        self,
+        scene: Scene,
+        x: int,
+        y: int,
+        path: str,
+        digits: int,
+    ):
+        width = 13 * digits
+        height = 23
+        super().__init__(scene, x, y, width, height)
+
+        self.path = path
+        self.digits = digits
+
+        self.nodes = []
+
+        self.gen_digits()
+    
+    def gen_digits(self):
+        for i in range(self.digits):
+            self.nodes.append(
+                AnimatedSprite(
+                    self.scene,
+                    self.x + i * 13,
+                    self.y,
+                    13,
+                    23,
+                    self.path,
+                    11),
+            )
+    
+    def update_digits(self, number: int):
+        digits = [int(d) for d in str(number).zfill(self.digits)]
+        
+        for i in range(len(digits)):
+            self.nodes[i].update_frame(digits[i])
+            self.nodes[i].animate()
--- src/bases/nodes/Timer.py
+++ src/bases/nodes/Timer.py
@@ -1,9 +1,9 @@
 # TYPES
-from src.bases.nodes.Node import Node
+from src.bases.nodes.Counter import Counter
 from src.bases.scenes.Scene import Scene
 from src.bases.nodes.AnimatedSprite import AnimatedSprite
 
-class Timer(Node):
+class Timer(Counter):
     def __init__(
         self,
         scene: Scene,
@@ -12,35 +12,11 @@ class Timer(Node):
         path: str,
         digits: int,
     ):
-        width = 13 * digits
-        height = 23
-        super().__init__(scene, x, y, width, height)
-
-        self.path = path
-        self.digits = digits
+        super().__init__(scene, x, y, path, digits)
 
         self.seconds = 0
-        self.nodes = []
         self.is_running = True
-
-        self.setup()
-
-    def setup(self):
         self.step_timer = self.scene.renderer.ticks
-        self.gen_digits()
-    
-    def gen_digits(self):
-        for i in range(self.digits):
-            self.nodes.append(
-                AnimatedSprite(
-                    self.scene,
-                    self.x + i * 13,
-                    self.y,
-                    13,
-                    23,
-                    self.path,
-                    11),
-            )
 
     def run(self):
         if self.seconds >= 999:
@@ -52,14 +28,7 @@ class Timer(Node):
             self.step_timer = now
             self.seconds += 1
 
-            self.update_digits()
-    
-    def update_digits(self):
-        digits = [int(d) for d in str(self.seconds).zfill(self.digits)]
-        
-        for i in range(len(digits)):
-            self.nodes[i].update_frame(digits[i])
-            self.nodes[i].animate()
+            self.update_digits(self.seconds)
     
     def stop(self):
         self.is_running = False
